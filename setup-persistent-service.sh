e74607b123b12e6cac2a61ebbc528b4a569b0984
Tom Zacharoff <warelock2000@protonmail.com>
2025-05-08 03:42:53 -0700

    Minor edit to boot time start setup

--- setup-persistent-service.sh
+++ setup-persistent-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -
+sudo cp social_game_typology_quiz.service /etc/systemd/system/
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 sudo systemctl enable social_game_typology_quiz.service
