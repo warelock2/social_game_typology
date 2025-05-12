2958c31dec56d930c920412cc56fd60dc74ccc00
Tom Zacharoff <warelock2000@protonmail.com>
2025-05-11 20:00:52 -0700

    I forgot to explicitly pass url_for through render_template_string

--- app/main.py
+++ app/main.py
@@ -252,10 +252,10 @@ def index():
             <p>
             If you would like to share this website, you can scan the QR code below:<p>
             <p>
-            <img src="{{ url_for('static', filename='images/quiz_website_qr_code.png') }}" alt="My Image">
+            <img src="{{ url_for('static', filename='images/quiz_website_qr_code.png') }}" alt="Quiz Website QR Code">
         </body>
     </html>
-    """)
+    """, url_for=url_for)
 
 @app.route('/quiz', methods=['GET'])
 def quiz():
