ee4e3154608153513737c1a0ae89bc31c7b6c568
Tom Zacharoff <warelock2000@protonmail.com>
2025-05-11 20:17:33 -0700

    Minor edits

--- app/main.py
+++ app/main.py
@@ -324,5 +324,5 @@ def results():
     return full_content
 
 if __name__ == '__main__':
-    app.run(ssl_context=('certs/cert.pem', 'certs/key.pem'), port=8443)
+    app.run(ssl_context=('certs/cert.pem', 'certs/key.pem'), port=3443)
 
