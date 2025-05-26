caa3c648475cdfd9b4060eafcc69b9ff7aff33f1
Tom Zacharoff <warelock2000@protonmail.com>
2025-05-26 16:12:11 -0700

    Upgraded role titles to include the term 'social'

--- app/main.py
+++ app/main.py
@@ -254,8 +254,8 @@ def index():
             <p>
             <img src="{{ url_for('static', filename='images/quiz_website_qr_code.png') }}" alt="Quiz Website QR Code"><p>
             <p>
-            UPDATE (2025-05-14): The old domain name for this website was: 
-            <pre>https://socialtypologyquiz.afobl.com</pre>
+            UPDATE (2025-05-14): The old domain name for this website was: <a href="https://socialtypologyquiz.afobl.com">https://socialtypologyquiz.afobl.com</a>.<p>
+            <p>
             It has since been renamed to <a href="https://socialgametypologyquiz.afobl.com">https://socialgametypologyquiz.afobl.com</a>, instead. The old domain name will become defunct shortly, so please update your web browser bookmarks accordingly. Thank you.
         </body>
     </html>
